9d3dc40cbcba73143f8d66b1bb05892b8840cca8
thejrengineer <148842502+thejrengineer@users.noreply.github.com>
2024-12-18 18:47:58 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home'; // Your main component with college listings
 import ReviewsPage from './components/Review'; // The new reviews page
-
+import Landing from './components/Landing'
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={< Landing/>} />
+                <Route path="/home" element={<Home />} />
                 <Route path="/college/:collegeId/reviews" element={<ReviewsPage />} />
             </Routes>
         </Router>
